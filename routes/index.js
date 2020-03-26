82a0eb5d5c6af3215bf973cf6ea0eb008fffcb19
Alan luu <luumanhquan.91@gmail.com>
2020-03-26 17:34:39 +0700

    sua lai loi duong dan them home

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/keiki-shop/', function(req, res, next) {
+router.get('/keiki-shop/home', function(req, res, next) {
     res.render('index');
 });
 
